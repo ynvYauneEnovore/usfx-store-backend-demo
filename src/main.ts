72575257c9a01a969807de7ce1d28764a9485bf1
ynvYauneEnovore <yovanuxf@gmail.com>
2023-10-20 20:21:19 -0400

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Configura CORS
+  app.enableCors();
 
   const config = new DocumentBuilder()
-  .setTitle('Usfx-Store')
-  .setDescription('respuesta a frontend tienda usfx-store')
-  .setVersion('1.0')
-  .build();
+    .setTitle('Usfx-Store')
+    .setDescription('Respuesta a frontend tienda usfx-store')
+    .setVersion('1.0')
+    .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
+
 bootstrap();
